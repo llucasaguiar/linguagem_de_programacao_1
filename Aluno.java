fed584ab7ab953f18f8f6b7ff379c0395e0a523f
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-09-29 20:59:29 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,48 @@
+public class Aluno extends Pessoa {
+    private int matricula;
+    private double nota1, nota2, nota3, media;
+
+    public void calcularMedia() {
+        media = (nota1+nota2+nota3)/3;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Pessoa p = new Pessoa();
+        Aluno a = new Aluno();
+
+        System.out.print("Nome completo: ");
+        p.setNome(sc.nextLine());
+
+        System.out.print("Idade: ");
+        p.setIdade(sc.nextInt());
+
+        System.out.println("Matrícula: ");
+        a.setMatricula(sc.nextInt());
+
+        System.out.println("Nota 1: ");
+        a.setNota1(sc.nextDouble());
+
+        System.out.println("Nota 2: ");
+        a.setNota2(sc.nextDouble());
+
+        System.out.println("Nota 3: ");
+        a.setNota3(sc.nextDouble());
+
+        System.out.println("Nome completo: " + p.getNome());
+        System.out.println("Idade: " + p.getIdade());
+        System.out.println("Matrícula: " + a.getMatricula());
+        System.out.println("Notas 1, 2 e 3 respectivamente: " + a.getNota1() + "; " + a.getNota2() + "; " + a.getNota3());
+        // linha para fazer o cálculo da média e depois exibí-la.
+        a.calcularMedia();
+        System.out.println("Média: " + a.getMedia());
+
+
+    }
+}
